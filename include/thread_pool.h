85b6e9cfbebe9f3ebc0dcb48fbff8ab9f4978e16
Kyy13 <kyy1393@gmail.com>
2022-04-21 00:17:05 -0400

    moved thread_pool to src

--- include/thread_pool.h
+++ include/thread_pool.h
@@ -80,17 +80,7 @@ namespace k13
         // Constructor
         // num_threads: number of threads in thread pool
         // sleep_duration: time for each thread to sleep when no tasks are available
-        thread_pool(size_t num_threads, std::chrono::milliseconds sleep_duration)
-            : m_running(true)
-            , m_sleep_duration(sleep_duration)
-        {
-            // Launch and store thread handles
-            m_threads.reserve(num_threads);
-            for (size_t i = 0; i != num_threads; ++i)
-            {
-                m_threads.push_back(std::make_unique<std::thread>(&thread_pool::thread_loop, this));
-            }
-        }
+        thread_pool(size_t num_threads, std::chrono::milliseconds sleep_duration);
 
         // Copy Constructor
         thread_pool(const thread_pool&) = delete;
@@ -99,57 +89,10 @@ namespace k13
         const thread_pool& operator=(const thread_pool&) = delete;
 
         // Destructor
-        ~thread_pool()
-        {
-            // Stop threads
-            m_running = false;
-            for (auto& pthread : m_threads)
-            {
-                pthread->join();
-            }
-        }
+        ~thread_pool();
 
         // Run a new task
-        void run(thread_task& task, std::function<void()> func)
-        {
-            // Create a pointer to task synchronization variables
-            auto& sync = task.m_sync;
-
-            if (!m_threads.empty())
-            {
-                std::lock_guard<std::mutex> lock(m_queue_mtx);
-
-                // Add task to queue
-                m_queue.push([sync = sync, func = std::move(func)]
-                {
-                    // Call the embedded function
-                    try
-                    {
-                        func();
-                    }
-                    catch(...)
-                    {
-                        sync->exception = std::current_exception();
-                    }
-
-                    // Tell main thread that the task has been processed
-                    sync->mtx.lock();
-                    sync->status = thread_task_complete;
-
-                    // Manual unlocking is done before notifying, to avoid waking up
-                    // the waiting thread only to block again (see notify_one for details)
-                    sync->mtx.unlock();
-                    sync->cv.notify_one();
-                });
-            }
-            else
-            {
-                // No threads in thread pool, call function immediately,
-                // and return completed thread_task
-                func();
-                sync->status = thread_task_complete;
-            }
-        }
+        void run(thread_task& task, std::function<void()> func);
 
     protected:
 
@@ -160,32 +103,7 @@ namespace k13
         std::chrono::milliseconds m_sleep_duration;
 
         // Per-thread loop
-        void thread_loop()
-        {
-            while (m_running)
-            {
-                std::function<void()> func;
-
-                // Get next task
-                m_queue_mtx.lock();
-                if (!m_queue.empty())
-                {
-                    func = std::move(m_queue.front());
-                    m_queue.pop();
-                }
-                m_queue_mtx.unlock();
-
-                // Run function, or sleep if there is no task
-                if (func)
-                {
-                    func();
-                }
-                else
-                {
-                    std::this_thread::sleep_for(m_sleep_duration);
-                }
-            }
-        }
+        void thread_loop();
     };
 
 }
--- src/thread_pool.cpp
+++ src/thread_pool.cpp
@@ -51,4 +51,94 @@ namespace k13
 
         return status != thread_task_progress;
     }
+
+    thread_pool::thread_pool(size_t num_threads, std::chrono::milliseconds sleep_duration)
+        : m_running(true)
+        , m_sleep_duration(sleep_duration)
+    {
+        // Launch and store thread handles
+        m_threads.reserve(num_threads);
+        for (size_t i = 0; i != num_threads; ++i)
+        {
+            m_threads.push_back(std::make_unique<std::thread>(&thread_pool::thread_loop, this));
+        }
+    }
+
+    thread_pool::~thread_pool()
+    {
+        // Stop threads
+        m_running = false;
+        for (auto& pthread : m_threads)
+        {
+            pthread->join();
+        }
+    }
+
+    void thread_pool::run(thread_task& task, std::function<void()> func)
+    {
+        // Create a pointer to task synchronization variables
+        auto& sync = task.m_sync;
+
+        if (!m_threads.empty())
+        {
+            std::lock_guard<std::mutex> lock(m_queue_mtx);
+
+            // Add task to queue
+            m_queue.push([sync = sync, func = std::move(func)]
+            {
+                // Call the embedded function
+                try
+                {
+                    func();
+                }
+                catch(...)
+                {
+                    sync->exception = std::current_exception();
+                }
+
+                // Tell main thread that the task has been processed
+                sync->mtx.lock();
+                sync->status = thread_task_complete;
+
+                // Manual unlocking is done before notifying, to avoid waking up
+                // the waiting thread only to block again (see notify_one for details)
+                sync->mtx.unlock();
+                sync->cv.notify_one();
+            });
+        }
+        else
+        {
+            // No threads in thread pool, call function immediately,
+            // and return completed thread_task
+            func();
+            sync->status = thread_task_complete;
+        }
+    }
+
+    void thread_pool::thread_loop()
+    {
+        while (m_running)
+        {
+            std::function<void()> func;
+
+            // Get next task
+            m_queue_mtx.lock();
+            if (!m_queue.empty())
+            {
+                func = std::move(m_queue.front());
+                m_queue.pop();
+            }
+            m_queue_mtx.unlock();
+
+            // Run function, or sleep if there is no task
+            if (func)
+            {
+                func();
+            }
+            else
+            {
+                std::this_thread::sleep_for(m_sleep_duration);
+            }
+        }
+    }
 }
